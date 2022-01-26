ecbe4200dfc4f106f7c00fbda81fc6f61651bacf
Quan <quantrung286@gmail.com>
2022-01-26 23:20:02 +0700

    up

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeader = {
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': '806b4ed36bmsh97d96b82cdb67c8p146942jsn2b0952e5ccb0',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeader })
+
+export const cryptoApi = createApi({
+	reducerPath: 'cryptoApi',
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: () => createRequest('/exchanges'),
+		}),
+	}),
+})
