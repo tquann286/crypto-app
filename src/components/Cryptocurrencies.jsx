24860296d6194bab079a340a30b60b99e1fb866e
Quan <quantrung286@gmail.com>
2022-01-27 00:24:06 +0700

    up

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import millify from 'millify'
+import { Card, Row, Col, Input } from 'antd'
+
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
+	const { data: cryptosList, isFetching } = useGetCryptosQuery()
+	const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+
+	console.log(cryptos)
+
 	return (
-    <div>
-      Cryptocurrencies
-    </div>
-  )
+		<React.Fragment>
+			<Row gutters={[32, 32]} className='crypto-card-container'></Row>
+		</React.Fragment>
+	)
 }
 
 export default Cryptocurrencies
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,12 +4,15 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import {Cryptocurrencies, News} from '../components';
 
 const { Title } = Typography
 
 const Homepage = () => {
 	const { data, isFetching } = useGetCryptosQuery()
-	console.log(data)
+	const globalStats = data?.data?.stats
+
+	if (isFetching) return 'Loading...'
 
 	return (
 		<React.Fragment>
@@ -18,21 +21,51 @@ const Homepage = () => {
 			</Title>
 			<Row>
 				<Col span={12}>
-					<Statistic title='Total Cryptocurrencies' value='5' />
+					<Statistic title='Total Cryptocurrencies' value={globalStats.total} />
 				</Col>
 				<Col span={12}>
-					<Statistic title='Total Exchanges' value='5' />
+					<Statistic
+						title='Total Exchanges'
+						value={millify(globalStats.totalExchanges)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title='Total Market Cap' value='5' />
+					<Statistic
+						title='Total Market Cap'
+						value={millify(globalStats.totalMarketCap)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title='Total 24h Volumn' value='5' />
+					<Statistic
+						title='Total 24h Volume'
+						value={millify(globalStats.total24hVolume)}
+					/>
 				</Col>
 				<Col span={12}>
-					<Statistic title='Total Markets' value='5' />
+					<Statistic
+						title='Total Markets'
+						value={millify(globalStats.totalMarkets)}
+					/>
 				</Col>
 			</Row>
+			<div className='home-heading-container'>
+				<Title level={2} className='home-title'>
+					Top 10 Cryptocurrencies in the world
+				</Title>
+				<Title level={3} className='show-more'>
+					<Link to='/cryptocurrencies'>Show More</Link>
+				</Title>
+			</div>
+			<Cryptocurrencies simplified />
+			<div className='home-heading-container'>
+				<Title level={2} className='home-title'>
+					Lastest Crypto News
+				</Title>
+				<Title level={3} className='show-more'>
+					<Link to='/news'>Show More</Link>
+				</Title>
+			</div>
+			<News simplified />
 		</React.Fragment>
 	)
 }
