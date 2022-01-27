56876a57fdc844a8f5c15c720acc5a3c318af958
Quan <quantrung286@gmail.com>
2022-01-27 11:24:15 +0700

    up

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Select,Typography,Row,Col,Avatar,Card} from 'antd'
 import moment from 'moment'
 
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+
 const News = () => {
 	return (
     <div>
