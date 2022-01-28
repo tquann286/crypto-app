793958bd98d6453370800fc8ed2a43037db20036
Quan <quantrung286@gmail.com>
2022-01-28 11:36:51 +0700

    up

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HTMLReactParser from 'html-react-parser'
+import { useParams } from 'react-router-dom'
+import millify from 'millify'
+import { Col, Row, Typography, Select } from 'antd'
+import {
+	MoneyCollectOutlined,
+	DollarCircleOutlined,
+	FundOutlined,
+	ExclamationCircleOutlined,
+	StopOutlined,
+	TrophyOutlined,
+	CheckOutlined,
+	NumberOutlined,
+	ThunderboltOutlined,
+} from '@ant-design/icons'
+
+const { Title, Text } = Typography
+const { Option } = Select
 
 const CryptoDetails = () => {
-	return (
-    <div>
-      CryptoDetails
-    </div>
-  )
+	const { coinId } = useParams()
+	const [timePeriod, setTimePeriod] = useState('7d')
+
+	return <div>CryptoDetails {coinId} </div>
 }
 
 export default CryptoDetails
