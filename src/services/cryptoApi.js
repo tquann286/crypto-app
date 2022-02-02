8df3f6c36efacc3c8209713930520f9e67ad1ca6
Quan <quantrung286@gmail.com>
2022-02-02 23:32:20 +0700

    up

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
 	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-	'x-rapidapi-key': '806b4ed36bmsh97d96b82cdb67c8p146942jsn2b0952e5ccb0',
+	'x-rapidapi-key': process.env.REACT_APP_API_KEY,
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
